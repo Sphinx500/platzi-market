ce415933ad4add2c78f76da841e5d13a99159f3c
Sphinx500 <1719210003@utectulancingo.edu.mx>
2022-05-19 17:01:08 -0500

    Query Methods

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -5,12 +5,20 @@ import com.platzi.market.persistence.entity.Producto;
 
 import java.util.List;
 
-public class ProductoRepository {
+import java.util.Optional;
 
+public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
-    //Metodo que recupere los productos
-    public List<Producto> getAll(){
+    public List<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
